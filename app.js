e7cc6054a3c8336d58ce06beb7c81a0262d60297
Hadi Safari <sa.hadisafari@gmail.com>
2021-12-14 22:00:29 -0800

    fix console logs to set item to local storage

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 
 document.getElementById("currentDay").innerText = moment().format('MMMM Do YYYY, h:mm:ss a');
 
-document.getElementById("btn").addEventListener("click", function (event) {
-  console.log(event.target.classList[0])
-  console.log(document.getElementById(event.target.classList[0]).value)
-  console.log(document.getElementById(event.target.classList[1]).innerText)
+var currentTime = moment().hours()
 
+
+document.getElementById("btn").addEventListener("click", function (event) {
+
+  var inputValue = document.getElementById(event.target.classList[0]).value
+
+  console.log(inputValue)
+
+  var divKey = localStorage.setItem(divKey, inputValue)
 })
+
+
+
+
 
